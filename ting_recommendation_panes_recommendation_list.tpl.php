427b3730e3abaf39c3bae8176ab7ff19be9a8f10
yusuf amer <impara1@gmail.com>
2012-11-16 10:31:48 +0100

    Added classes for styling in tpl's

--- ting_recommendation_panes_recommendation_list.tpl.php
+++ ting_recommendation_panes_recommendation_list.tpl.php
@@ -1,8 +1,7 @@
 <?php if ($models && (sizeof($models) > 0)) :?>
-<h3><?php print t("Others borrowed:"); ?></h3>
-<ul class="list simple">
+<ul class="list adhl">
   <?php foreach ($models as $i => $model) : ?>
-    <li class="<?php echo (($i % 2) == 0) ? 'odd' : 'even' ?>">
+    <li class="list-item <?php echo (($i % 2) == 0) ? 'odd' : 'even' ?>">
       <?php echo theme('ting_recommendation_panes_recommendation_list_entry', array("model" => $model)); ?>
     </li>
   <?php endforeach; ?>
--- ting_recommendation_panes_recommendation_list_entry.tpl.php
+++ ting_recommendation_panes_recommendation_list_entry.tpl.php
@@ -6,6 +6,6 @@ $title = $model['title'];
 <a href="<?php echo $url ?>" title="<?php print check_plain($title) ?><?php if (!empty($creators_string)) { print ': ' . check_plain($creators_string); } ?>">
   <span class="title"><?php print check_plain($title); ?></span>
   <?php if (!empty($creators_string)) { ?>
-    <span class="creator"><?php print check_plain($creators_string); ?></span>
+    <span class="creator label"><?php print check_plain($creators_string); ?></span>
   <?php } ?>
 </a>
